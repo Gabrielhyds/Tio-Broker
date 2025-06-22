565f58bd70aa2c5bf5dd098648b8e5059292fd0f
Gabrielhyds <starkstory6@gmail.com>
2025-06-22 18:05:18 -0300

    [52] - ajustando config db

--- config/config.php
+++ config/config.php
@@ -1,25 +1,24 @@
 <?php
+
+// Conexão com banco de dados
+// Faz a conexão com o banco de dados.
+
+// Nesse bloco de codigo vamos definir as variaveis que serão usadas para fazer a conexão com o banco de dados
+
 $host = "localhost";
 $databasename = "tio_Broker";
 $username = "root";
+$password = "";
 
-// Tenta conectar sem senha
-$senhaTentada = "root";
-$connection = @new mysqli($host, $username, $senhaTentada, $databasename);
+// Nesse proximo bloco ele cria uma conexão com o banco de dados 
+// aqui usamos uma função do PHP para conectar o banco chamada "mysqli"
 
+$connection = new mysqli($host, $username, $password, $databasename);
+
+// Nesse utimo passso verifica se ocorreu algum erro na conexão com o PHP e o Banco de dados
+//usaremos um if 
+//caso entre no if ele exibe uma mensagem do erro que causou
 if ($connection->connect_error) {
-    // Tenta com senha padrão
-    $senhaTentada = "root";
-    $connection = @new mysqli($host, $username, $senhaTentada, $databasename);
-
-    if ($connection->connect_error) {
-        // Se falhar novamente, exibe o erro final
-        die("Erro de conexão com MySQL: " . $connection->connect_error);
-    } else {
-        echo "✅ Conectado com senha padrão ('$senhaTentada').";
-    }
-} else {
-    //echo "✅ Conectado sem senha.";
+    //exibe a mensagem de erro e encerra o script
+    die("Erro de conexão:" . $connection->connect_error);
 }
-
-// A partir daqui, sua conexão $connection está pronta para uso.
