54c245ce2aae103eb5e5104adc4385ef275edb86
Gabrielhyds <starkstory6@gmail.com>
2025-06-22 18:13:08 -0300

    [56] - ajustando config db tentativa #5

--- config/config.php
+++ config/config.php
@@ -1,23 +1,24 @@
 <?php
+
+// Conexão com banco de dados
+// Faz a conexão com o banco de dados.
+
+// Nesse bloco de codigo vamos definir as variaveis que serão usadas para fazer a conexão com o banco de dados
+
 $host = "localhost";
 $databasename = "tio_Broker";
 $username = "root";
-$senhas = ["root", ""];
+$password = "";
 
-$connection = null;
+// Nesse proximo bloco ele cria uma conexão com o banco de dados 
+// aqui usamos uma função do PHP para conectar o banco chamada "mysqli"
 
-foreach ($senhas as $senha) {
-    $conn = @new mysqli($host, $username, $senha, $databasename);
-    if (!$conn->connect_error) {
-        $connection = $conn;
-        define('DB_PASSWORD_USADA', $senha);
-        break;
-    }
+$connection = new mysqli($host, $username, $password, $databasename);
+
+// Nesse utimo passso verifica se ocorreu algum erro na conexão com o PHP e o Banco de dados
+//usaremos um if 
+//caso entre no if ele exibe uma mensagem do erro que causou
+if ($connection->connect_error) {
+    //exibe a mensagem de erro e encerra o script
+    die("Erro de conexão:" . $connection->connect_error);
 }
-
-if (!$connection) {
-    die("❌ Erro ao conectar no banco de dados: " . $conn->connect_error);
-}
-
-// Expondo como global (opcional, mas útil)
-$GLOBALS['connection'] = $connection;
