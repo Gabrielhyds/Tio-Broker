51a856758370db5500808c08877b9b156a6e9539
Gabrielhyds <starkstory6@gmail.com>
2025-06-22 20:01:46 -0300

    [62] - crontab

--- config/config.php
+++ config/config.php
@@ -11,7 +11,7 @@ $username = "root";
 $password = "";
 
 
-//atenção
+//atenção crontab ativado
 // Nesse proximo bloco ele cria uma conexão com o banco de dados 
 // aqui usamos uma função do PHP para conectar o banco chamada "mysqli"
 
