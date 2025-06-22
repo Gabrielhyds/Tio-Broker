b1269d5c0fb6889a03f31536a7314383564b3fda
Gabrielhyds <starkstory6@gmail.com>
2025-06-21 23:32:25 -0300

    [45] - ajustando rota

--- config/rotas.php
+++ config/rotas.php
@@ -1,4 +1,7 @@
 <?php
 $basePath = '/tio-broker/';
-$temApp = is_dir(__DIR__ . '/../views'); // Corrigido: verifica onde REALMENTE está a pasta views
+
+// Verifica se a pasta 'app' e 'views' existem
+$temApp = is_dir(__DIR__ . '/../views'); // Caminho relativo a 'app/config/'
+
 define('BASE_URL', $basePath . ($temApp ? 'app/' : ''));
