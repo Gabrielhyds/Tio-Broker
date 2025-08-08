8a9a852e674c61538cfd6f1caffbb2e65456f368
Gabrielhyds <starkstory6@gmail.com>
2025-08-08 19:47:06 -0300

    refactor: add favicon link to authentication pages for improved branding

--- views/auth/resetar_senha.php
+++ views/auth/resetar_senha.php
@@ -9,6 +9,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="../assets/img/favicon.png">
     <style>
         body {
             font-family: 'Inter', sans-serif;
